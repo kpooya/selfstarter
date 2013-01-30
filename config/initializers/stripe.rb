11bf4b8b19ccf323c05e90d5e33de07a3a7c22b9
john <qiang@gust.com>
2013-01-29 18:58:48 -0800

    Deploy live stripe keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,11 +1,7 @@
 if Rails.env.production?
-  Stripe.api_key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-  STRIPE_PUBLISHABLE_KEY = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  Stripe.api_key = ENV["STRIPE_API_KEY"]
+  STRIPE_PUBLISHABLE_KEY = "pk_live_zQopWHOhNqlucOhpVt4HRblw"
 else
   Stripe.api_key = "sk_test_nOsPS1Sgmw2yC1RfQvjoklWO"
   STRIPE_PUBLISHABLE_KEY = "pk_test_K9t4awU0QmOsGVt2OkFKA5dw"
 end
-
-# STRIPE_PLAN_ID = "some-awesome-plan"
-# STRIPE_TRIAL_PERIOD = 1.month# For test
-
