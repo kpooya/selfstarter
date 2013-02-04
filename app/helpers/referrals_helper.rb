4fc1585c5f5c54a09163a41063c3d21ee5fa686a
john <qiang@gust.com>
2013-02-04 13:07:25 -0800

    Mark string as html safe

--- app/helpers/referrals_helper.rb
+++ app/helpers/referrals_helper.rb
@@ -2,6 +2,7 @@ module ReferralsHelper
   def get_tweet_query_string(referring_url)
     referring_url_encoded = URI.escape(@referring_url)
 
-    "original_referer=" + referring_url_encoded + "&text=" + URI.escape("Never Lose Any Valuables! Help make it happen for Linquet Mini. ") + referring_url_encoded + " #cool #RT";
+    query_string = "original_referer=" + referring_url_encoded + "&text=" + URI.escape("Never Lose Any Valuables! Help make it happen for Linquet Mini. ") + referring_url_encoded + " #cool #RT";
+    query_string.html_safe
   end
 end
